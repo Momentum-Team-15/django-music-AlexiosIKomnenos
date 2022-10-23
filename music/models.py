86f2798a7f4e4c9c57260a6f4d2e0e275280bf97
AlexiosIKomnenos <106697127+AlexiosIKomnenos@users.noreply.github.com>
2022-10-23 14:09:13 -0400

    close but not there

--- music/models.py
+++ music/models.py
@@ -10,6 +10,7 @@ class Album(models.Model):
     artist = models.ForeignKey('Artist', on_delete=models.CASCADE, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey('User', on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return f"{self.title} by {self.artist}"
--- music/views.py
+++ music/views.py
@@ -1,4 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Album
+from music.forms import AlbumForm
 
 def index(request): 
-    return render(request, 'music/index.html')
+    albums = Album.objects.all()
+    return render(request, 'music/index.html', {'albums': albums})
+
+def album_detail(request, pk): 
+    albums = Album.objects.get(pk=pk)
+    return render(request, 'music/album_detail.html', {'album': albums})
+
+def create_album(request): 
+    if request.method == 'POST':
+        form = AlbumForm(request.POST)
+        if form.is_valid():
+            album = form.save()
+        
+            return redirect("home")
+
+            
+    else: 
+        form = AlbumForm()
+        # if user is visting a page with GET request, not submitting the form yet
+    return render(request, 'music/create_album.html', {'form': form})
